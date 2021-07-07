dc7d2998267df64b1b60c2202fa692b7725b9089
Grigorii Shestakov <gregshestakov@gmail.com>
2021-07-07 17:07:00 +0300

    map change in test 1

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -13,16 +13,16 @@ fun shoppingListCost(
     shoppingList: List<String>,
     costs: Map<String, Double>
 ): Double {
-    var totalCost = 0.0
+    var total23Cost = 0.0
 
     for (item in shoppingList) {
         val itemCost = costs[item]
         if (itemCost != null) {
-            totalCost += itemCost
+            total23Cost += itemCost
         }
     }
 
-    return totalCost
+    return total23Cost
 }
 
 /**
